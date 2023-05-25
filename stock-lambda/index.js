af66ea018fb30aad4adef2921d6651a4a6879fc1
JEONGDratini <pw050503@gmail.com>
2023-05-25 15:44:11 +0900

    day2 completegit add .!

---
+++ stock-lambda/index.js
@@ -0,0 +1,12 @@
+
+const consumer = async (event) => {
+
+  for (const record of event.Records) {
+    const messageAttributes = record.messageAttributes;
+    console.log(
+      "Message Attribute: ",
+      messageAttributes.AttributeName.stringValue
+    );
+    console.log("Message Body: ", record.body);
+  }
+};
